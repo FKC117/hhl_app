f3916c360a91faabbe5a8971f724c85c9fe74aa4
FKC 117 <fazlul.karim117@gmail.com>
2026-06-29 11:51:57 +0600

    base url updated

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/foundation.dart';
 
 class ApiConfig {
+  static const _defaultApiBaseUrl = String.fromEnvironment(
+    'HHL_API_BASE_URL',
+    defaultValue: 'https://hhl.analyticabd.xyz/api/v1',
+  );
+
   static String get apiBaseUrl {
-    if (kIsWeb) {
-      return 'http://127.0.0.1:8000/api/v1';
+    final configured = _defaultApiBaseUrl.trim();
+    if (configured.isNotEmpty) {
+      return configured;
     }
 
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return 'http://192.168.0.102:8000/api/v1';
-      default:
-        return 'http://127.0.0.1:8000/api/v1';
-    }
+    return 'https://hhl.analyticabd.xyz/api/v1';
   }
 
   static String get apiRootUrl {
